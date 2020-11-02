b55f22f29a01af270f037dc16409cb048a740965
cosmin turtureanu <cosminturtureanu1234@gmail.com>
2020-11-02 20:38:14 +0200

    adding support for timer

--- src/components/App.js
+++ src/components/App.js
@@ -7,30 +7,37 @@ import gameEventsTemp from './../data/gameEvents.json';
 import { useState } from 'react';
 
 export default function App() {
-  const [gameEvents, setGameEvents] = useState(Array.from(gameEventsTemp)); // to copy the gameEvents as it's read only
+  const [gameEvents] = useState(Array.from(gameEventsTemp)); // to copy the gameEvents as it's read only
   const statsCount = 4;
   const statsMaxLevel = 10;
+  const timerIncrementRate = 366 / gameEventsTemp.length;
 
   const [state, setState] = useState({
     statsLevel: Array(statsCount).fill(statsMaxLevel / 2),
     statsLastLevel: Array(statsCount).fill(0),
+    updateTimer: false,
+    timerPassed: 0,
   });
 
 	const sendLevel = effect => setState({
     statsLevel: state.statsLevel.map((level, index) => level + effect[index]), // no need to call Array.from() bc a new array is returned
     statsLastLevel: Array.from(state.statsLevel),
+    updateTimer: true,
+    timerPassed: state.timerPassed + timerIncrementRate,
   });
-  
+
   const removeCard = () => {
     gameEvents.splice(0, 1);
     // setGameEvents(Array.from(gameEvents)); // for some reason this causes a memory leak but the app works without it
   }
+  
+
 
   return (
     <div className="app">
       <GameStats statsInfo={state} statsMaxLevel={statsMaxLevel}/>
       <CardsContainer sendLevel={sendLevel} gameEvents={gameEvents} removeCard={removeCard}/>
-      <Timer eventsCount={gameEventsTemp.length}/>
+      <Timer passed={state.timerPassed}/>
     </div>
   );
 }
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,21 +1,10 @@
 import './../css/Timer.css';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 export default function Timer(props) {
-  /* const incrementRate = 365 / props.eventsCount;
-  const [passed, setPassed] = useState(0);
-  const [incrementTimer, setIncrementTimer] = useState(true);
-  
-  console.log('rebuilt');
-  
-   if(incrementTimer) {
-     setIncrementTimer(false);
-     setPassed(passed + incrementRate);
-   } */
-   
   return (
     <div className="timer">
-      {/*passed*/} zile au trecut
+      {Math.round(props.passed)} zile au trecut
     </div>
   );
 }
